a059542c1e8a28d4a19012eeb92a649bfa8c4e3b
Leonardo <Leonardoaraujo97@outlook.com>
2019-09-06 20:18:38 -0300

    Fixing the first end-point

--- src/main/java/com/Skynet/teacher/service/CursoService.java
+++ src/main/java/com/Skynet/teacher/service/CursoService.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import com.Skynet.teacher.entities.Curso;
 import com.Skynet.teacher.repository.CursoRepository;
 
+@Service
 public class CursoService {
 
 	@Autowired
